75fe0d28ab20b1883d098aec08604cfcbf81f1be
AndrewDeras <andrewderas7@gmail.com>
2023-05-31 21:52:56 -0300

    react-base ready

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { Router } from 'react-router-dom/cjs/react-router-dom.min';
 import { ToastContainer } from 'react-toastify';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 
-import store from './store';
+import store, { persistor } from './store';
 import GlobalStyles from './styles/GlobalStyles';
 import history from './services/history';
 import Routes from './routes';
@@ -11,12 +12,14 @@ import Header from './components/Header';
 function App() {
   return (
     <Provider store={store}>
-      <Router history={history}>
-        <Header />
-        <Routes />
-        <GlobalStyles />
-        <ToastContainer autoClose={4000} className="toast-container" />
-      </Router>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Header />
+          <Routes />
+          <GlobalStyles />
+          <ToastContainer autoClose={4000} className="toast-container" />
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
